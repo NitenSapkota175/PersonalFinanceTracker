aa1dd70e33c3cff9ef4f857006a210b3ba19e08b
NitenSapkota175 <nitensapkota1705@gmail.com>
2024-07-31 00:14:48 +0530

    create an add function in main.py to intialize the csv file and to get the value from data_entry module

--- data_entry.py
+++ data_entry.py
@@ -18,7 +18,7 @@ def get_amount():
         amount = float(input("Enter the amount : "))
         if amount <= 0:
             raise ValueError("Amount must be a non-negative non-zero value.")
-            return amount
+        return amount
     except ValueError as e:
         print(e)
         return get_amount()
--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 import pandas as pd
 import csv
 from datetime import datetime
-
+from data_entry import get_date,get_amount,get_category,get_description
 class CSV:
-    CSV_FILE = "finance_data.csv"
+    CSV_FILE = "finance.csv"
     COLUMNS = ["date","amount","category","description"]
     @classmethod
     def initialize_csv(cls):
@@ -29,5 +29,12 @@ class CSV:
         print("Entry aded successfully")
 
 
-CSV.initialize_csv()
-# CSV.add_entry("30-07-2024",100,"Expense","Hotel room cost")    
+def add():
+        CSV.initialize_csv()
+        date = get_date("Enter the date of the transaction (dd-mm-yyyy) or enter for today's date : ",allow_default=True)
+        amount = get_amount()
+        category = get_category()
+        description = get_description()
+        CSV.add_entry(date,amount,category,description)
+        
+add()    
